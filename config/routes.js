16db32e2e7598514e5f7c4fb535de6557619ccc1
wuyanhua0514@163.com <wuyanhua0514@163.com>
2019-10-22 11:42:53 +0800

    页面初始化

---
+++ config/routes.js
@@ -0,0 +1,47 @@
+const Routes= [
+    {
+      path: '/user',
+      component: '../layouts/UserLayout',
+      routes: [
+        {
+          name: 'login',
+          path: '/user/login',
+          component: './user/login',
+        },
+      ],
+    },
+    {
+      path: '/',
+      component: '../layouts/SecurityLayout',
+      routes: [
+        {
+          path: '/',
+          component: '../layouts/BasicLayout',
+          authority: ['admin', 'user'],
+          routes: [
+            {
+              path: '/',
+              redirect: '/welcome',
+            },
+            {
+              path: '/welcome',
+              name: 'welcome',
+              icon: 'smile',
+              component: './Welcome',
+            },
+            {
+              component: './404',
+            },
+          ],
+        },
+        {
+          component: './404',
+        },
+      ],
+    },
+    {
+      component: './404',
+    },
+  ]
+
+  export default Routes;
